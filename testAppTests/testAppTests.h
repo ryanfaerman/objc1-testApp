09f1a555885d687ea9fba0834acb8585d5c45c79
Ryan Faerman <ryan@faerman.net>
2012-05-03 21:03:16 -0400

    Initial Commit

---
+++ testAppTests/testAppTests.h
@@ -0,0 +1,13 @@
+//
+//  testAppTests.h
+//  testAppTests
+//
+//  Created by Ryan Faerman on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testAppTests : SenTestCase
+
+@end
---
+++ testAppTests/testAppTests.m
@@ -0,0 +1,32 @@
+//
+//  testAppTests.m
+//  testAppTests
+//
+//  Created by Ryan Faerman on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "testAppTests.h"
+
+@implementation testAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testAppTests");
+}
+
+@end
